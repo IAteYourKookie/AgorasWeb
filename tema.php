0156c2f7b15a72a36c886de44f643b5f64408f4c
IAteYourKookie <loorenachan@gmail.com>
2021-12-21 22:57:28 -0300

    retirei a verificação do php para teste

--- tema.php
+++ tema.php
@@ -13,27 +13,24 @@ $titulo = NULL;
 $descricao = NULL;
 
 //check for required fields
-if (isset($_POST['titulo']) && isset($_POST['descricao'])) {
-    $titulo = trim($_POST['titulo']);
-    $descricao = trim($_POST['descricao']);
 
-    // insert temas 
-    $result = pg_query($bdOpen, "INSERT INTO tema(titulo, descricao, FK_USUARIOS_id_usuario) 
-    SELECT tema.titulo, tema.descricao tema.FK_USUARIOS_id_usuario 
-    FROM usuarios INNER JOIN tema 
-    ON tema.FK_USUARIOS_id_usuarios = usuarios.FK_USUARIOS_id_usuarios");
+$titulo = trim($_POST['titulo']);
+$descricao = trim($_POST['descricao']);
 
-    //check erro
-    if ($result) {
-        $response["success"] = 1;
-    } else {
-        $response["success"] = 0;
-        $response["error"] = "Error BD: " . pg_last_error($bdOpen);
-    }
+// insert temas 
+$result = pg_query($bdOpen, "INSERT INTO tema(titulo, descricao, FK_USUARIOS_id_usuario) 
+SELECT tema.titulo, tema.descricao tema.FK_USUARIOS_id_usuario 
+FROM usuarios INNER JOIN tema 
+ON tema.FK_USUARIOS_id_usuarios = usuarios.FK_USUARIOS_id_usuarios");
+
+//check erro
+if ($result) {
+    $response["success"] = 1;
 } else {
     $response["success"] = 0;
-    $response["error"] = "campo em branco";
+    $response["error"] = "Error BD: " . pg_last_error($bdOpen);
 }
+
 
 pg_close($bdOpen);
 echo json_encode($response);
