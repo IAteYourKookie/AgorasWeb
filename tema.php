0c0f4053e53d136b9a0bcfa7f7469f3c078895f7
IAteYourKookie <loorenachan@gmail.com>
2021-12-30 18:11:37 -0300

    inserindo dados

--- tema.php
+++ tema.php
@@ -18,11 +18,11 @@ $titulo = trim($_POST['titulo']);
 $descricao = trim($_POST['descricao']);
 
 // insert temas 
-$result = pg_query($bdOpen, "INSERT INTO tema(titulo, descricao, fk_usuarios_id_usuario) 
-SELECT tema.titulo, tema.descricao, tema.fk_usuarios_id_usuario 
+$result = pg_query($bdOpen, "INSERT INTO tema(fk_usuarios_id_usuario) 
+SELECT tema.fk_usuarios_id_usuario 
 FROM usuarios INNER JOIN tema 
 ON tema.fk_usuarios_id_usuario = usuarios.id_usuario;");
-$result= pg_query($bdOpen,"INSERT INTO tema(titulo,descricao) VALUES('$titulo','$descricao')");
+
 
 //check erro
 if ($result) {
