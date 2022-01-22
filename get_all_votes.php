d60113cb9997ed8caeb05919b2c7f4235b4d2fab
Shayane <shayaneteixeira56@gmail.com>
2022-01-22 00:15:26 -0300

    add get_all_votes.php e update em get_all_coments.php

--- get_all_votes.php
+++ get_all_votes.php
@@ -12,31 +12,22 @@ require "./configs/conexao.php";
 $response = array();
  
 // Realiza uma consulta ao BD e obtem todos os produtos.
-$result = pg_query($bdOpen, "SELECT * FROM comentario");
+$result = pg_query($bdOpen, "SELECT * FROM curtida");
  
 
 if (pg_num_rows($result) > 0) {
     // Caso existam produtos no BD, eles sao armazenados na 
 	// chave "products". O valor dessa chave e formado por um 
 	// array onde cada elemento e um produto.
-    $response["comentario"] = array();
+    $response[""] = array(); //aqui dentro estava comentario, o que colocar para pegar a curtida
  
     while ($row = pg_fetch_array($result)) {
-        // Para cada produto, sao retornados somente o 
-		// pid (id do produto) e o nome do produto. Nao ha necessidade 
-		// de retornar nesse momento todos os campos de todos os produtos 
-		// pois a app cliente, inicialmente, so precisa do nome do mesmo para 
-		// exibir na lista de produtos. O campo pid e usado pela app cliente 
-		// para buscar os detalhes de um produto especifico quando o usuario 
-		// o seleciona. Esse tipo de estrategia poupa banda de rede, uma vez 
-		// os detalhes de um produto somente serao transferidos ao cliente 
-		// em caso de real interesse.
-        $comentario = array();
-        $comentario["id_comentario"] = $row["id_comentario"];
-        $comentario["comentario"] = $row["comentario"];
+        $curtida = array();
+        $curtida["id_curtida"] = $row["id_curtida"];
+        $curtida[""] = $row[""];
  
         // Adiciona o produto no array de produtos.
-        array_push($response["comentario"], $comentario);
+        array_push($response[""], $comentario); 
     }
     // Caso haja produtos no BD, o cliente 
 	// recebe a chave "success" com valor 1.
@@ -52,7 +43,7 @@ if (pg_num_rows($result) > 0) {
 	// recebe a chave "success" com valor 0. A chave "message" indica o 
 	// motivo da falha.
     $response["success"] = 0;
-    $response["message"] = "Nao ha comentarios";
+    $response["message"] = "Nao ha curtidas";
 	
 	// Fecha a conexao com o BD
 	pg_close($bdOpen);
@@ -60,3 +51,4 @@ if (pg_num_rows($result) > 0) {
     // Converte a resposta para o formato JSON.
     echo json_encode($response);
 }
+?>
