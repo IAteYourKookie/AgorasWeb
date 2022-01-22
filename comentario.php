0dacfa2e6868b1e6d257721078f034951aaf6595
Shayane <shayaneteixeira56@gmail.com>
2022-01-21 21:41:32 -0300

    testando pela 1021 vez

--- comentario.php
+++ comentario.php
@@ -7,16 +7,10 @@ $response = array();
 $comentario = NULL;
 
 //check for required fields
-$login = trim($_GET['login']);
-
-
+$login = trim($_POST['login']);
 $comentario = trim($_POST['comentario']);
 
-$id=pg_query($bdOpen, "(SELECT id_usuario from usuario where email='$login')");
-print_r($login);
-echo "<br>";
-print_r($id);
-$result = pg_query($bdOpen, "INSERT INTO comentario(data_envio, comentario, fk_usuario_id_usuario) VALUES(NOW(), '$comentario',$id);");
+$result = pg_query($bdOpen, "INSERT INTO comentario(data_envio, comentario, fk_usuario_id_usuario) VALUES(NOW(), '$comentario',(SELECT id_usuario from usuario where email='$login'));");
 
 /*
 $result = pg_query($bdOpen, "INSERT INTO comentario(data_envio, comentario, fk_usuario_id_usuario)
@@ -33,7 +27,6 @@ FROM usuario INNER JOIN comentario ON comentario.id_comentario = usuario.fk_usua
 //check erro
 if ($result) {
     $response["success"] = 1;
-    return;
 } else {
     $response["success"] = 0;
     $response["error"] = "Error BD: " . pg_last_error($bdOpen);
