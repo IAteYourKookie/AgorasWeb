15b4fc27a6f5cfeb30f65d605d666e8eba7ec058
IAteYourKookie <loorenachan@gmail.com>
2022-01-23 16:23:13 -0300

    editei get perfil

--- get_perfil.php
+++ get_perfil.php
@@ -10,15 +10,16 @@ $login = trim($_POST['login']);
 $query = pg_query($bdOpen, "SELECT * FROM usuario WHERE email='$login')");
 $result = pg_fetch_array($query);
 
-$nome = $result['nome'];
-$usuario = $result['nome_de_usuario'];
-$email = $result['email'];
-$bio = $result['bio']; 
-$imgPfp = $result['pfp']; // imagem atual 
+$response["usuario"] = array();
+while ($row = pg_fetch_array($result)) {
+    $usuario = array();
+    $usuario['naome'] = $row['nome'];
+    $usuario['userName'] = $row['nome_de_usuario'];
+    $usuario['bio'] = $row['bio'];
+    $usuario['img']=$row['pfp'];
+}
 
-//check erro
-if ($result) {
-    $response["success"] = 1;
+$response["success"] = 1;
 } else {
     $response["success"] = 0;
     $response["error"] = "Error BD: " . pg_last_error($bdOpen);
