44cab5b369a512c8194d8053474d07a4ef11361f
IAteYourKookie <loorenachan@gmail.com>
2022-01-23 17:10:12 -0300

    testes

--- get_perfil.php
+++ get_perfil.php
@@ -8,6 +8,7 @@ $response = array();
 //session de usuario
 $login = trim($_POST['login']);
 $result = pg_fetch_array($bdOpen, "SELECT * FROM usuario WHERE email='$login')");
+echo $result;
 
 if (pg_num_rows($result) > 0) {
     $response["usuario"] = array();
