95aa3e7aa2b6c3b4df0a446aa0b99616053729e1
IAteYourKookie <loorenachan@gmail.com>
2022-01-23 19:05:44 -0300

    editar perfil

--- editPerfil.php
+++ editPerfil.php
@@ -16,8 +16,8 @@ $newEmail = trim($_POST['editEmail']);//novo email
 $newBio = trim($_POST['editBio']);//bio
 $img = trim($_POST['editImg']);//foto perfil
 
-$imageFileType = strtolower(pathinfo(basename($_FILES["img"]["name"]),PATHINFO_EXTENSION));
-$image_base64 = base64_encode(file_get_contents($_FILES['img']['tmp_name']) );
+//$imageFileType = strtolower(pathinfo(basename($_FILES["img"]["name"]),PATHINFO_EXTENSION));
+//$image_base64 = base64_encode(file_get_contents($_FILES['img']['tmp_name']) );
 $img = 'data:image/'.$imageFileType.';base64,'.$image_base64;
 
 
