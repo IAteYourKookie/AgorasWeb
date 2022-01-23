6ef67c022fc87c436e1966999b2d22aeb2d375ba
Shayane <shayaneteixeira56@gmail.com>
2022-01-22 21:59:44 -0300

    teste

--- deleteUser.php
+++ deleteUser.php
@@ -11,35 +11,31 @@ $query = pg_query($bdOpen, "SELECT id_usuario FROM usuario WHERE email='$login'"
 $row = pg_fetch_array($query);
 $id_usuario = $row[0];
 
-$query = pg_query($bdOpen, "SELECT * FROM resposta WHERE fk_usuario_id_usuario = '$id_usuario'");
+$query = pg_query($bdOpen, "SELECT * FROM resposta WHERE fk_usuario_id_usuario = $id_usuario");
 $row = pg_fetch_array($query);
 if ($row) {
-    echo "entrou 1";
-    pg_query($bdOpen, "DELETE FROM resposta WHERE fk_usuario_id_usuario = '$id_usuario'");
+    pg_query($bdOpen, "DELETE FROM resposta WHERE fk_usuario_id_usuario = $id_usuario");
 }
 
-$query = pg_query($bdOpen, "SELECT * FROM comentario WHERE fk_usuario_id_usuario = '$id_usuario'");
+$query = pg_query($bdOpen, "SELECT * FROM comentario WHERE fk_usuario_id_usuario = $id_usuario");
 $row = pg_fetch_array($query);
 if ($row) {
-    echo "entrou 2";
-    pg_query($bdOpen, "DELETE FROM comentario WHERE fk_usuario_id_usuario = '$id_usuario'");
+    pg_query($bdOpen, "DELETE FROM comentario WHERE fk_usuario_id_usuario = $id_usuario");
 }
 
-$query = pg_query($bdOpen, "SELECT * FROM curtida WHERE fk_usuario_id_usuario = '$id_usuario'");
+$query = pg_query($bdOpen, "SELECT * FROM curtida WHERE fk_usuario_id_usuario = $id_usuario");
 $row = pg_fetch_array($query);
 if ($row) {
-    echo "entrou 3";
-    pg_query($bdOpen, "DELETE FROM curtida WHERE fk_usuario_id_usuario = '$id_usuario'");
+    pg_query($bdOpen, "DELETE FROM curtida WHERE fk_usuario_id_usuario = $id_usuario");
 }
 
-$query = pg_query($bdOpen, "SELECT * FROM tema WHERE fk_usuario_id_usuario = '$id_usuario'");
+$query = pg_query($bdOpen, "SELECT * FROM tema WHERE fk_usuario_id_usuario = $id_usuario");
 $row = pg_fetch_array($query);
 if ($row) {
-    echo "entrou 4";
-    pg_query($bdOpen, "DELETE FROM tema WHERE fk_usuario_id_usuario = '$id_usuario'");
+    pg_query($bdOpen, "DELETE FROM tema WHERE fk_usuario_id_usuario = $id_usuario");
 }
 
-$result = pg_query($bdOpen, "DELETE FROM usuario WHERE id_usuario = '$id_usuario'");
+$result = pg_query($bdOpen, "DELETE FROM usuario WHERE id_usuario = $id_usuario");
 
 
 //check erro
