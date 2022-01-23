e1da2bf6485cae16fc62c66954b3c1b1dc240c6b
IAteYourKookie <loorenachan@gmail.com>
2022-01-23 16:59:46 -0300

    testes

--- get_perfil.php
+++ get_perfil.php
@@ -7,15 +7,18 @@ $response = array();
 
 //session de usuario
 $login = trim($_POST['login']);
-$query = pg_query($bdOpen, "SELECT * FROM usuario WHERE email='$login')");
-$result = pg_fetch_array($query);
+$result = pg_query($bdOpen, "SELECT * FROM usuario WHERE email='$login')");
 
 if (pg_num_rows($result) > 0) {
     $response["usuario"] = array();
-
+    
     while ($row = pg_fetch_array($result)) {
         $usuario = array();
-        
+        $usuario['name'] = $row['nome'];
+        $usuario['userName'] = $row['nome_de_usuario'];
+        $usuario['bio'] = $row['bio'];
+        $usuario['img']=$row['pfp'];
+        array_push($response["usuario"]);
     }
     $response["success"] = 1;
 }else {
