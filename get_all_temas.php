0c750c7ff4a19c2239639170d0eb540510d511d1
IAteYourKookie <loorenachan@gmail.com>
2022-01-24 13:44:56 -0300

    atualizacoes

--- get_all_temas.php
+++ get_all_temas.php
@@ -14,39 +14,46 @@ require "./configs/conexao.php";
 $response = array();
  
 // Realiza uma consulta ao BD e obtem todos os produtos.
+$login = trim($_GET['login']);
 $result = pg_query($bdOpen, "SELECT * FROM tema");
  
+if (isset($_GET['login'])) {
+    if (pg_num_rows($result) > 0) {
+        // Caso existam produtos no BD, eles sao armazenados na 
+        // chave "products". O valor dessa chave e formado por um 
+        // array onde cada elemento e um produto.
+        $response["tema"] = array(); 
+    
+        while ($row = pg_fetch_array($result)) {
+            $tema = array();
+            $tema["id_tema"] = $row["id_tema"];
+            $tema["titulo"] = $row["titulo"];
+            $tema["desc"] = $row["descricao"];
+            $tema["id_usuario"] = $row["fk_usuario_id_usuario"];
 
-if (pg_num_rows($result) > 0) {
-    // Caso existam produtos no BD, eles sao armazenados na 
-	// chave "products". O valor dessa chave e formado por um 
-	// array onde cada elemento e um produto.
-    $response["tema"] = array(); 
- 
-    while ($row = pg_fetch_array($result)) {
-        $tema = array();
-        $tema["id_tema"] = $row["id_tema"];
-        $tema["titulo"] = $row["titulo"];
-        $tema["desc"] = $row["descricao"];
-        $tema["id_usuario"] = $row["fk_usuario_id_usuario"];
- 
-        // Adiciona o produto no array de produtos.
-        array_push($response["tema"], $tema); 
+            $query = pg_query($bdOpen, "SELECT nome FROM usuario WHERE id_usuario='$tema['id_usuario']'");
+            $row = pg_fetch_array($query);
+            $row = $row['nome'];
+            $tema["nomeUsuario"] = $row;
+    
+            // Adiciona o produto no array de produtos.
+            array_push($response["tema"], $tema); 
+        }
+        // Caso haja produtos no BD, o cliente 
+        // recebe a chave "success" com valor 1.
+        $response["success"] = 1;
+        
+    } else {
+        // Caso nao haja produtos no BD, o cliente 
+        // recebe a chave "success" com valor 0. A chave "message" indica o 
+        // motivo da falha.
+        $response["success"] = 0;
+        $response["message"] = "Nao ha temas";
     }
-    // Caso haja produtos no BD, o cliente 
-	// recebe a chave "success" com valor 1.
-    $response["success"] = 1;
-	
-} else {
-    // Caso nao haja produtos no BD, o cliente 
-	// recebe a chave "success" com valor 0. A chave "message" indica o 
-	// motivo da falha.
-    $response["success"] = 0;
-    $response["message"] = "Nao ha temas";
-	
-
+    pg_close($bdOpen);
+    echo json_encode($response);
+}else{
+    pg_close($bdOpen);
+    echo json_encode($response);
 }
-pg_close($bdOpen);
-echo json_encode($response);
-
 ?>
