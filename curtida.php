4e41a8aa7b7e98b9b36a06b854d3f9034d113c13
IAteYourKookie <loorenachan@gmail.com>
2022-01-26 20:39:37 -0300

    teste

--- curtida.php
+++ curtida.php
@@ -15,40 +15,47 @@ quando o usuário clicar no botão de curtida, é preciso verificar qual o estad
 provavelmente são feitos bastante pelo AndroidStudio, mas vou fazer a lógica aqui 
 */
 
-// verificar se tem algum registro de usuario e do tema na tabela curtida
 
-$id_usuario = 1; //generico, para teste, o real é o id do usuario cadastrado no momento
-$id_tema = 1; //generico, para teste, o real é o tema em que ele está votando
+if (isset($_POST['login'])){
+
+    // verificar se tem algum registro de usuario e do tema na tabela curtida
+    $login = trim($_POST['login']);
+    $query = pg_query($bdOpen, "SELECT id_usuario from usuario where email='$login'");
+    $row = pg_fetch_array($query);
+    $id_tema = $row['id_usuario'];
 
 
-$sql = "SELECT (fk_usuario_id_usuario, fk_tema_id_tema)
-FROM curtida 
-WHERE fk_usuario_id_usuario = $id_usuario 
-AND fk_tema_id_tema = $id_tema;";
-$result = pg_query($bdOpen, $sql);
+    $sql = "SELECT (fk_usuario_id_usuario, fk_tema_id_tema)
+    FROM curtida 
+    WHERE fk_usuario_id_usuario = $id_usuario 
+    AND fk_tema_id_tema = $id_tema;";
+    $result = pg_query($bdOpen, $sql);
 
-if (pg_fetch_array($sql)) {
-    $del = pg_query($bdOpen, "DELETE FROM curtida WHERE fk_usuario_id_usuario = $id_usuario AND FK_TEMA_id_tema = $id_tema");
-    
-    // mudar o botão para 'não curtido'
+    if (pg_fetch_array($sql)>0) {
+        $del = pg_query($bdOpen, "DELETE FROM curtida WHERE fk_usuario_id_usuario = $id_usuario AND FK_TEMA_id_tema = $id_tema");
+        
+        // mudar o botão para 'não curtido'
 
-} else { 
-    // pegar o id de usuario e o de debate
-    $result = pg_query($bdOpen, "INSERT INTO curtida(fk_usuario_id_usuario, fk_tema_id_tema) VALUES ($id_usuario, $id_tema)"); 
+    } else { 
+        // pegar o id de usuario e o de debate
+        $result = pg_query($bdOpen, "INSERT INTO curtida(fk_usuario_id_usuario, fk_tema_id_tema) VALUES ($id_usuario, $id_tema)"); 
 
-    // mudar o botão para 'curtido'
-} 
+        // mudar o botão para 'curtido'
+    } 
 
-//check erro
-if ($result) {
-    $response["success"] = 1;
-    $response["message"] = "Curtida alterada com sucesso";
+    //check erro
+    if ($result) {
+        $response["success"] = 1;
+        $response["message"] = "Curtida alterada com sucesso";
 
-} else {
-    $response["success"] = 0;
-    $response["error"] = "Error BD: " . pg_last_error($bdOpen);
+    } else {
+        $response["success"] = 0;
+        $response["error"] = "Error BD: " . pg_last_error($bdOpen);
+    }
+    pg_close($bdOpen);
+    echo json_encode($response);
+}else{
+    pg_close($bdOpen);
+    echo json_encode($response);
 }
-
-pg_close($bdOpen);
-echo json_encode($response);
 ?>
