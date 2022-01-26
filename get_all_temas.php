6422f9c9a4a1f51e7c0c25df2969bc3921c12c6f
IAteYourKookie <loorenachan@gmail.com>
2022-01-26 19:06:40 -0300

    teste

--- get_all_temas.php
+++ get_all_temas.php
@@ -2,7 +2,7 @@
 require "./configs/conexao.php"; 
 
  /*
- * O seguinte codigo retorna para o cliente a lista de produtos 
+ * O seguinte codigo retorna para o cliente a lista de temas 
  * armazenados no servidor. Essa e uma requisicao do tipo GET. 
  * Nao sao necessarios nenhum tipo de parametro.
  * A resposta e no formato JSON.
@@ -13,8 +13,10 @@ require "./configs/conexao.php";
 // array que guarda a resposta da requisicao
 $response = array();
  
-// Realiza uma consulta ao BD e obtem todos os produtos.
-$result = pg_query($bdOpen, "SELECT t.id_tema, t.titulo, t.descricao, 
+// Realiza uma consulta ao BD e obtem todos os temas.
+$result = pg_query($bdOpen, "SELECT * FROM tema");
+
+/*$result = pg_query($bdOpen, "SELECT t.id_tema, t.titulo, t.descricao, 
 t.fk_usuario_id_usuario, COUNT(*) as curtidas 
 FROM curtida as c 
 LEFT JOIN debate as d
@@ -22,11 +24,11 @@ ON c.fk_tema_id_tema = d.fk_tema_id_tema
 INNER JOIN tema as t 
 ON c.fk_tema_id_tema = t.id_tema
 WHERE d.FK_TEMA_id_tema is null
-GROUP BY t.id_tema ORDER BY COUNT(*) DESC");
+GROUP BY t.id_tema ORDER BY COUNT(*) DESC");*/
  
 
 if (pg_num_rows($result) > 0) {
-    // Caso existam produtos no BD, eles sao armazenados na 
+    // Caso existam temas no BD, eles sao armazenados na 
     // chave "products". O valor dessa chave e formado por um 
     // array onde cada elemento e um produto.
     $response["tema"] = array(); 
@@ -44,15 +46,15 @@ if (pg_num_rows($result) > 0) {
         $row = $row['nome'];
         $tema["nomeUsuario"] = $row;
     
-        // Adiciona o produto no array de produtos.
+        // Adiciona o produto no array de temas.
         array_push($response["tema"], $tema); 
     }
-    // Caso haja produtos no BD, o cliente 
+    // Caso haja temas no BD, o cliente 
     // recebe a chave "success" com valor 1.
     $response["success"] = 1;
         
 } else {
-    // Caso nao haja produtos no BD, o cliente 
+    // Caso nao haja temas no BD, o cliente 
     // recebe a chave "success" com valor 0. A chave "message" indica o 
     // motivo da falha.
     $response["success"] = 0;
